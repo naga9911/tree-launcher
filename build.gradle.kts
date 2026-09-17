41a188b4b6ffe92db87064040d390e124de953c8
unknown <nagababu.self@gmail.com>
2026-09-17 03:21:17 -0700

    Update extra property syntax in root build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,13 @@
 buildscript {
-    ext {
-        compose_ui_version = "1.5.4"
-        kotlin_version = "1.9.0"
+    extra.apply {
+        set("composeUiVersion", "1.5.4")
+        set("kotlinVersion", "1.9.0")
     }
 }
+
+val composeUiVersion: String by extra
+val kotlinVersion: String by extra
+
 plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
